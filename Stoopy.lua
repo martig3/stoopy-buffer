16183b602bfbc8e52037fbfee4ff07af1676067b
MartinWienc <32913968+MartinWienc@users.noreply.github.com>
2022-08-14 17:46:44 -0400

    impl basic input queue display & logic

--- Stoopy.lua
+++ Stoopy.lua
@@ -1,16 +1,44 @@
 print("loading Stoopy Buffer")
 local EventFrame = CreateFrame("frame", "EventFrame")
 EventFrame:RegisterEvent("UNIT_SPELLCAST_SENT")
-local frame = CreateFrame("Frame", nil, UIParent, 'TooltipBackdropTemplate')
-frame:SetSize(100, 100)
-frame:SetPoint("CENTER", 200, 0)
-frame:SetMovable(true)
+
+local container = CreateFrame("Frame", nil, UIParent, 'TooltipBackDropTemplate')
+container:SetSize(300, 100)
+container:SetPoint("CENTER", 400, 0)
+container:SetMovable(true)
+container:EnableMouse(true)
+container:RegisterForDrag("LeftButton")
+container:SetScript("OnDragStart", function(self)
+  self:StartMoving()
+end)
+container:SetScript("OnDragStop", function(self)
+  self:StopMovingOrSizing()
+end)
+
+local buffer = {}
+for i = 1, 3, 1 do
+  local frame = CreateFrame("Frame", nil, container, nil)
+  frame:Show()
+  frame:SetPoint("CENTER", (85 * i) - 170, 0)
+  frame:SetSize(80, 80)
+  local bg = frame:CreateTexture('bg', 'ARTWORK', nil, -6)
+  bg:SetAllPoints(frame)
+  buffer[i] = bg
+end
+
+local function pushIcon(icon, i)
+  local bg = buffer[i]
+  local currentTexture = bg:GetTexture()
+  bg:SetTexture(icon)
+  if buffer[i + 1] == nil or currentTexture == nil then return end
+  pushIcon(currentTexture, i + 1)
+end
+
 EventFrame:SetScript("OnEvent", function(self, event, ...)
-        if (event == "UNIT_SPELLCAST_SENT") then
-                local unit, target, castGUID, spellID = ...
-                local name, _, icon = GetSpellInfo(spellID)
-                local tex = frame:CreateTexture(nil, "BACKGROUND")
-                tex:SetTexture(icon)
-                tex:SetAllPoints(frame) --make texture same size as button
-        end
+  if (event == "UNIT_SPELLCAST_SENT") then
+    local unit, target, castGUID, spellID = ...
+    local name, _, icon = GetSpellInfo(spellID)
+    pushIcon(icon, 1)
+    print(icon)
+  end
 end)
