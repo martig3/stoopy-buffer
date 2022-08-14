20bb01cc0313963f5ecdd7bb32a1bdc5c3311677
MartinWienc <32913968+MartinWienc@users.noreply.github.com>
2022-08-14 18:35:40 -0400

    added logic for detecting user spamming keys

--- Stoopy.lua
+++ Stoopy.lua
@@ -1,6 +1,6 @@
-print("loading Stoopy Buffer")
 local EventFrame = CreateFrame("frame", "EventFrame")
 EventFrame:RegisterEvent("UNIT_SPELLCAST_SENT")
+EventFrame:RegisterEvent("UNIT_SPELLCAST_FAILED")
 
 local container = CreateFrame("Frame", nil, UIParent, 'TooltipBackDropTemplate')
 container:SetSize(300, 100)
@@ -15,7 +15,11 @@ container:SetScript("OnDragStop", function(self)
   self:StopMovingOrSizing()
 end)
 
-local buffer = {}
+local bgs = {}
+local icons = {}
+local frames = {}
+local timings = {}
+local spamCount = 0
 for i = 1, 3, 1 do
   local frame = CreateFrame("Frame", nil, container, nil)
   frame:Show()
@@ -23,22 +27,39 @@ for i = 1, 3, 1 do
   frame:SetSize(80, 80)
   local bg = frame:CreateTexture('bg', 'ARTWORK', nil, -6)
   bg:SetAllPoints(frame)
-  buffer[i] = bg
+  bgs[i] = bg
+  frames[i] = frame
 end
 
 local function pushIcon(icon, i)
-  local bg = buffer[i]
+  local bg = bgs[i]
+  if bg == nil then return end
   local currentTexture = bg:GetTexture()
   bg:SetTexture(icon)
-  if buffer[i + 1] == nil or currentTexture == nil then return end
+  if currentTexture == nil then return end
   pushIcon(currentTexture, i + 1)
+end
+
+local function handleSpamming()
+  spamCount = spamCount + 1
+  print(spamCount)
 end
 
 EventFrame:SetScript("OnEvent", function(self, event, ...)
   if (event == "UNIT_SPELLCAST_SENT") then
     local unit, target, castGUID, spellID = ...
     local name, _, icon = GetSpellInfo(spellID)
+    if icons[1] == icon then handleSpamming() return end
+    if event == 'UNIT_SPELLCAST_FAILED' then return end
+    icons[1] = icon
     pushIcon(icon, 1)
     print(icon)
   end
+  if event == "UNIT_SPELLCAST_FAILED" then
+    local _, _, spellID = ...
+    local name, _, icon = GetSpellInfo(spellID)
+    if icons[1] == icon then handleSpamming() return end
+  end
 end)
+
+print("Loaded Stoopy Buffer")
